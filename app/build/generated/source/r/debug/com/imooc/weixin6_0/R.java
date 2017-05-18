3e2a6e04f695b4ca5013be1c8900c0a38c76046e
chenyousheng <chenyousheng@cd.baidu.com>
2017-05-18 11:29:22 +0800

    Go

--- app/build/generated/source/r/debug/com/imooc/weixin6_0/R.java
+++ app/build/generated/source/r/debug/com/imooc/weixin6_0/R.java
@@ -146,6 +146,11 @@ containing a value of this type.
     }
     public static final class style {
         /**  API 11 theme customizations can go here. 
+
+            Theme customizations available in newer API levels can go in
+            res/values-vXX/styles.xml, while customizations related to
+            backward-compatibility can go here.
+        
          */
         public static final int AppBaseTheme=0x7f050000;
         public static final int AppTheme=0x7f050003;
